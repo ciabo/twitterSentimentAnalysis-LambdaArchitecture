8e9fbcea5b85dddb89f3e72c95050258db61dcf8
ettore <ettorecelozzi@gmail.com>
2019-02-18 16:46:32 +0100

    Batch sentiment analysis

--- src/main/java/masterdataset/MDatasetQuery.java
+++ src/main/java/masterdataset/MDatasetQuery.java
@@ -13,15 +13,6 @@ import java.util.HashMap;
 import java.util.List;
 
 public class MDatasetQuery {
-    static List ages;
-
-    public MDatasetQuery() {
-        ages = Arrays.asList(
-                Arrays.asList("ettore", 22),
-                Arrays.asList("marco", 55),
-                Arrays.asList("luca", 18));
-    }
-
     public static void tweetProcessing(List tweet) {
         Api.execute(
                 new StdoutTap(),
