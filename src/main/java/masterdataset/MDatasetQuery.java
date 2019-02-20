b605a9fb14de49e05f2bbc89a61ef2c9f3f81695
ciabo <lucaciabo@hotmail.it>
2019-02-20 10:29:00 +0100

    bug fix

--- src/main/java/masterdataset/MDatasetQuery.java
+++ src/main/java/masterdataset/MDatasetQuery.java
@@ -1,6 +1,6 @@
 package masterdataset;
 
-import com.twitter.maple.tap.StdoutTap;
+
 import jcascalog.Api;
 import jcascalog.Playground;
 import jcascalog.Subquery;
@@ -14,22 +14,11 @@ import java.util.List;
 public class MDatasetQuery {
 
     public static void wordCount() {
-        Api.execute(
-                new StdoutTap(),
-                new Subquery("?word", "?count")
-                        .predicate(Playground.SENTENCE, "?sentence")
-                        .predicate(new Split(), "?sentence").out("?word")
-                        .predicate(new Count(), "?count"));
+
     }
 
     public static void tweetProcessing(List tweet) {
-        Api.execute(
-                new StdoutTap(),
-                new Subquery("?keyword", "?sentiment", "?count")
-                        .predicate(tweet, "?tweet")
-                        .predicate(new SentimentAnalysis(), "?tweet").out("?keyword", "?sentiment")
-                        .predicate(new Count(), "?count")
-        );
+
     }
 }
 
