93fa04d47b9d5b41b6ce8b741a5f12adf1dfbf37
ciabo <lucaciabo@hotmail.it>
2019-02-20 17:41:48 +0100

    helpppp

--- src/main/java/masterdataset/MDatasetQuery.java
+++ src/main/java/masterdataset/MDatasetQuery.java
@@ -10,14 +10,17 @@ import java.util.List;
 
 public class MDatasetQuery {
     public static void tweetProcessing(List tweet, List smr) {
+        Subquery queryresult = new Subquery("?keyword", "?sentiment", "?count")
+                .predicate(smr, "_", "?smr")
+                .predicate(new QueryResult(), "?keyword", "?sentiment", "?count", "?smr").out("?keyword", "?sentiment", "?count");
+
         Api.execute(
                 new StdoutTap(),
                 new Subquery("?keyword", "?sentiment", "?count")
                         .predicate(tweet, "?tweet")
                         .predicate(new SentimentAnalysis(), "?tweet").out("?keyword", "?sentiment")
                         .predicate(new Count(), "?count")
-                        .predicate(smr, "?smr")
-                        .predicate(new QueryResult(), "?keyword", "?sentiment", "?count", "?smr").out("?keyword", "?sentiment", "?count")
+                        .predicate(queryresult, "?keyword", "?sentiment", "?count")
         );
     }
 }
