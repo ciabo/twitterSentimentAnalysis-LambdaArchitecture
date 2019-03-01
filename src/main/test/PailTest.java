1477da9b91858c2107757d0894287992a35cfe79
ettore <ettorecelozzi@gmail.com>
2019-03-01 12:11:21 +0100

    Pail testing

--- src/main/test/PailTest.java
+++ src/main/test/PailTest.java
@@ -7,6 +7,8 @@ import org.apache.hadoop.fs.Path;
 import org.junit.*;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 public class PailTest {
     private static String path = "hdfs://localhost:9000/user/ettore/pail/tweet/data";
@@ -14,10 +16,13 @@ public class PailTest {
     private static Pail tweetPail;
     private static Pail newPail;
     private static FileSystem fs;
+    private static List<String> fulltweet;
 
     @BeforeClass
     public static void setUp() throws IOException {
+        fulltweet = Arrays.asList("02032019", "120822", "Sunny ddaaaaayyyy!!");
         fs = DataStore.configureHDFS();
+        fs.delete(new Path("/user/ettore/pail"), true);
         tweetPail = Pail.create(path, new TweetStructure());
         newPail = Pail.create(newpath, new TweetStructure());
     }
@@ -30,12 +35,20 @@ public class PailTest {
     @Test
     public void testBatchPail() throws IOException {
         DataStore.writeTweet(tweetPail, "1502019", "192133", "Team Giannis");
+        DataStore.writeTweet(tweetPail, "01032019", "121133", "Team Ettore");
+        DataStore.readTweet(path);
+    }
+
+    @Test
+    public void testBatchPailList() throws IOException {
+        DataStore.writeTweet(tweetPail, fulltweet.get(0), fulltweet.get(1), fulltweet.get(2));
         DataStore.readTweet(path);
     }
 
     @Test
     public void testNewDataPail() throws IOException {
         DataStore.writeTweet(newPail, "13022019", "155849", "Team Lebron");
+        DataStore.writeTweet(newPail, "01032019", "140547", "Team Luca");
         DataStore.readTweet(newpath);
     }
 
