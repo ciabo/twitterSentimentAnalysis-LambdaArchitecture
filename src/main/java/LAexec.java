df8b2809b919fa7d67b85372e31a99c61d84bdc3
Ettore <ettorecelozzi@gmail.com>
2019-03-11 17:40:38 +0100

    Last fixes

--- src/main/java/LAexec.java
+++ src/main/java/LAexec.java
@@ -4,17 +4,12 @@ import fastlayer.cassandra.SentimentRepository;
 import batchlayer.pail.DataStore;
 import batchlayer.jcascalog.MDatasetQuery;
 import batchlayer.pail.TweetStructure;
-import utils.Utils;
 import org.apache.hadoop.fs.FileSystem;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class LAexec {
-    private List batch = new ArrayList();
-    private List fast = new ArrayList();
     private MDatasetQuery mq;
     private Pail tweetPail;
     private Pail newTweetPail;
@@ -34,40 +29,6 @@ public class LAexec {
             }
         } catch (IOException e) {
         }
-    }
-
-    public void startLA(int endbatch, String filename) {
-        try {
-            BufferedReader br = new BufferedReader(new FileReader(filename));
-//            BufferedWriter bwB = new BufferedWriter(new FileWriter("dbBatch.txt"));
-//            BufferedWriter bwF = new BufferedWriter(new FileWriter("db15.txt"));
-            String line;
-            int count = 0;
-            while ((line = br.readLine()) != null && count < 15) {
-                String[] split = line.split(",");
-                String tweet = Utils.createTweet(Arrays.copyOfRange(split, 2, split.length));
-                if (count < endbatch) {
-                    batch.add(Arrays.asList(tweet));
-//                    bwB.append(tweet+"\n");
-                } else {
-                    fast.add(Arrays.asList(tweet));
-//                    bwF.append(tweet+"\n");
-                }
-                count++;
-            }
-//            bwB.close();
-//            bwF.close();
-        } catch (IOException e) {
-            System.out.println("Error with file " + filename);
-        }
-    }
-
-    public List<String> getBatch() {
-        return batch;
-    }
-
-    public List<String> getFast() {
-        return fast;
     }
 
     public void executeLA(FileSystem fs) throws IOException {
